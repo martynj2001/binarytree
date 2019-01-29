1a81cf01c9d78f2c6b5cc9a4eed06ae078a3db7a
martynj2001 <martynj2010@icloud.com>
2019-01-29 19:46:41 +0000

    Renamed Knight class to ChessPiece and made super class to future proof.

--- knight.rb
+++ knight.rb
@@ -1,13 +1,13 @@
 # The Odin Project, Data Structures and alogorithms asssignment pt II. The Knight chesspiece script.
 
 
-class Knight
+class ChessPiece
 
     attr_accessor :pos_x, :pos_y, :possible_moves
     attr_reader :moves
 
     def initialize (posistion)
-        @moves = [[1, 2], [1,-2], [-1, 2],[-1,-2],[2, 1],[2, -1],[-2, 1],[-2, -1]]
+        @moves = []
         @pos_x = posistion[0]
         @pos_y = posistion[1]
         @possible_moves = []
@@ -26,7 +26,7 @@ class Knight
         current_position = [pos_x, pos_y]
     end
 
-    def move_knight (new_posn)
+    def move_piece (new_posn)
         if @possible_moves.include?(new_posn)
             @pos_x = new_posn[0]
             @pos_y = new_posn[1]
@@ -34,10 +34,21 @@ class Knight
     end
 end
 
+class Knight < ChessPiece
+
+    def initialize (posistion)
+        @moves = [[1, 2], [1,-2], [-1, 2],[-1,-2],[2, 1],[2, -1],[-2, 1],[-2, -1]]
+        @pos_x = posistion[0]
+        @pos_y = posistion[1]
+        @possible_moves = []
+        set_possible_moves
+    end
+end
+
 knight = Knight.new([0,0]) 
 knight.possible_moves # => [[1, 2], [2, 1]]
 knight.current_position # => [0, 0]
-knight.move_knight ([1,2])
+knight.move_piece ([1,2])
 knight.current_position # => [1, 2]
 
 
