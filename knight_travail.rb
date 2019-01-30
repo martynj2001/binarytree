a860b36a24ec20865d1adbf26522068700acb0ac
martynj2001 <martynj2010@icloud.com>
2019-01-30 20:06:36 +0000

    Added basic Vertex and Graph classes.

--- knight_travail.rb
+++ knight_travail.rb
@@ -5,10 +5,34 @@ require_relative 'chesspiece'
 
 class Vertex
 
-    attr_accessor :piece
+    attr_accessor :piece :coord :neighbours :weight
+
+    def initialize (coordinates)
+        @coord = coordinates
+        @neighbours = []
+        @weights = []
+    end
 
 end
 
 class Graph
 
+    attr_accessor :vertices
+
+    def initialize
+        @vertices = []
+    end
+
+    def add_vertex(coordinates)
+        @vertices << Vertex.new(coordinates)
+    end
+
+    def find_vertex_by_coord (coordinates)
+        @vertices.each {|vertex| return vertex if vertex.coord == coordinates}
+        nil
+    end
+
+    def count
+        @vertices.length
+    end
 end
